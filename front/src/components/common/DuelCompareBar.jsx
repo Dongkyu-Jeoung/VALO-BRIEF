e6bf5b74b9c287b2c0e56c2269af9e93bc301061
DK <ehdzb28@naver.com>
2026-09-01 12:46:03 +0900

    프론트엔드 수정

--- front/src/components/common/DuelCompareBar.jsx
+++ front/src/components/common/DuelCompareBar.jsx
@@ -1,17 +1,29 @@
-export default function DuelCompareBar({ leftLabel, leftPct, rightLabel, rightPct }) {
+import React from 'react';
+
+export default function DuelCompareBar({ 
+  leftLabel = '우리팀', 
+  leftPct = 50, 
+  rightLabel = '상대팀', 
+  rightPct = 50 
+}) {
   return (
     <div className="duel-compare">
-      <div className="duel-side-label">
-        {leftLabel}
-        <b>{leftPct}%</b>
+      {/* 좌측: 우리팀 */}
+      <div className="duel-side-label us">
+        <span className="team-name">{leftLabel}</span>
+        <b className="team-pct">{leftPct}%</b>
       </div>
+
+      {/* 중앙 게이지 바 */}
       <div className="duel-bar">
         <div className="us" style={{ width: `${leftPct}%` }} />
         <div className="them" style={{ width: `${rightPct}%` }} />
       </div>
-      <div className="duel-side-label right">
-        {rightLabel}
-        <b>{rightPct}%</b>
+
+      {/* 우측: 상대팀 */}
+      <div className="duel-side-label them">
+        <span className="team-name">{rightLabel}</span>
+        <b className="team-pct">{rightPct}%</b>
       </div>
     </div>
   );
